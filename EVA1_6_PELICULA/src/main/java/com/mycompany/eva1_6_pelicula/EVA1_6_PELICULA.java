9a5da39c1f3eb3ec1013bffea0d05b39416b89fd
Armandooiou <L24550279@chihuahua2.tecnm.mx>
2024-09-05 16:24:50 -0600

    Add files via upload

---
+++ EVA1_6_PELICULA/src/main/java/com/mycompany/eva1_6_pelicula/EVA1_6_PELICULA.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_6_pelicula;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aaro1
+ */
+public class EVA1_6_PELICULA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+              //DECLARAMOS VARIABLES   
+    String Nombre;
+    String Genero;
+    int Duracion;
+    String Reparto;
+    String Clasificacion;
+    
+     Scanner captu;
+    captu = new Scanner(System.in);
+    System.out.println("introducir el nombre de la pelicula");
+    Nombre = captu.nextLine();
+    System.out.println(Nombre);
+    System.out.println("introducir el nombre de la pelicula");
+    Nombre = captu.nextLine();
+    System.out.println("introducir el genero de la pelicula");
+    Genero = captu.nextLine();
+    System.out.println("introducir la duracion de la pelicula");
+    Genero = captu.nextLine();
+    System.out.println("introducir el reparto de la pelicula");
+    Clasificacion = captu.nextLine();
+    System.out.println("introducir la clacificacion de la pelicula");
+    
+    //IMPRIMIR
+    
+        System.out.println("introducir el nombre de la pelicula");
+        System.out.println("introducir el nombre de la pelicula");
+        System.out.println("introducir el genero de la pelicula");
+        System.out.println("introducir la duracion de la pelicula");
+        System.out.println("introducir el reparto de la pelicula");
+        
+    
+    
+    
+    
+    }
+}
